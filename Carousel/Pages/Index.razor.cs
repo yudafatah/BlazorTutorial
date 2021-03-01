8799c9d41175a511ded8e13732375915bb20734e
fatah <yudafatah@gmail.com>
2021-03-01 13:47:07 +0700

    Add image slide / carousel materials

---
+++ Carousel/Pages/Index.razor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Carousel.Pages
+{
+    public partial class Index
+    {
+        // Image Slider
+        List<ImageSliderVM> items = new List<ImageSliderVM>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            // fetch list data Images
+            var res1 = await Http.GetFromJsonAsync<ImageSliderVM[]>("sample-data/carouselImg.json");
+            items.AddRange(res1.ToList());
+
+            //StateHasChanged();
+        }
+    }
+
+    public class ImageSliderVM
+    {
+        public string Source { get; set; } = "";
+        public string Alt { get; set; } = "";
+        public string Caption { get; set; } = "";
+        public string Header { get; set; } = "";
+        public string AppName { get; set; } = "";
+    }
+}
---
+++ Carousel/Program.cs
@@ -0,0 +1,29 @@
+using BlazorStrap;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carousel
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            // register BlazorStrap CSS
+            builder.Services.AddBootstrapCss();
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
